b8d024bc1d995bac8c3cd1143df0e7bb5db35643
railites <pardeepdhingra01@yahoo.com>
2014-05-07 15:59:55 +0530

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, 'KEY', 'SCERET'
   provider :twitter, 'KEY', 'SCERET'
   
- # provider :google, '888276678145-85kefr5qmho6g9g1k4pov2bggb9u1ptk.apps.googleusercontent.com', 'ZRd2dVF3JjypALx-0zwGeMRv', :scope => 'https://www.googleapis.com/auth/drive'
- 
  provider :google_oauth2, 'xxxxxxxxxxxxx.apps.googleusercontent.com', 'SCERET', { access_type: "offline", approval_prompt: "" }
   
 end
